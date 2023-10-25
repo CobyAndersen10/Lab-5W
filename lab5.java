6b7aac8180e35bf10923266511feefad9e93a1b0
CobyAndersen10 <143895280+CobyAndersen10@users.noreply.github.com>
2023-10-25 14:52:09 -0400

    Update lab5.java

--- lab5.java
+++ lab5.java
@@ -1,1 +1,13 @@
+public static class lab5 
 
+public static void main string(String[] args) {
+
+
+  BinarySearchTree bst = new myBinarySearchTree
+  int msti = new myTreeIterator
+  treeIterator iterator = new myTreeIterator
+
+    
+
+  }
+  
