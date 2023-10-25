2404e71e36f440d56b3d13bd0b1af77abb8f1d59
CobyAndersen10 <143895280+CobyAndersen10@users.noreply.github.com>
2023-10-25 17:00:50 -0400

    Update lab5.java

--- lab5.java
+++ lab5.java
@@ -1,9 +1,14 @@
 public static class lab5 
 
 public static void main string(String[] args) {
-
+//Create myBinarySearchTree object and add values
   myBinarySearchTree bst = new myBinarySearchTree();
+  bst.add(2);
+  bst.add(1);
+  bst.add(3);
+  // Creates myTreeIterator object
   myTreeIterator treeIterator = new myTreeIterator();
+  // Creates a TreeItertator Testing object, passes bst and treeIterator objects
   TreeIteratorTesting testing = new TreeIteratorTesting(bst, treeIterator);
 
   
