26654d1fe7018b72d4f194aa11da29a941c1082b
CobyAndersen10 <143895280+CobyAndersen10@users.noreply.github.com>
2023-10-25 14:59:55 -0400

    Update lab5.java

--- lab5.java
+++ lab5.java
@@ -2,11 +2,13 @@ public static class lab5
 
 public static void main string(String[] args) {
 
+  myBinarySearchTree bst = new myBinarySearchTree();
+  myTreeIterator treeIterator = new myTreeIterator();
+  TreeIteratorTesting testing = new TreeIteratorTesting(bst, treeIterator);
 
-  BinarySearchTree bst = new myBinarySearchTree
-  int msti = new myTreeIterator
-  treeIterator iterator = new myTreeIterator
+  
 
+  
     
 
   }
